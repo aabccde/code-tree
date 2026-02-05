01f1a96dc0f8faceef00539a393652d9db6138da
aabccde <112850083+aabccde@users.noreply.github.com>
2026-02-05 11:22:17 +0900

    Title: 점수 입출력, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260205/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		System.out.printf("Your score is %d point.", age);
+	}
+
+}
