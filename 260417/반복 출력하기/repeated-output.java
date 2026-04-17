4cfc59357c3dd267f23a873c90cd150e39c55c5e
aabccde <112850083+aabccde@users.noreply.github.com>
2026-04-17 09:40:14 +0900

    Title: 반복 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260417/반복 출력하기/repeated-output.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		print(n);
+		
+	}
+	
+	public static void print(int n) {
+		for (int i = 0; i<n; i++) {
+			System.out.println("12345^&*()_");
+		}
+	}
+
+}
